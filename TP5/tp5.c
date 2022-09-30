d3b745b58c5fa5504b4de779afeccde8561ef91e
LBF38 <mathis.urien@free.fr>
2022-09-30 11:34:19 +0200

    TP5 Reformatage du fichier

--- TP5/tp5.c
+++ TP5/tp5.c
@@ -12,47 +12,44 @@ struct cell
     struct cell *next; // pourquoi met on un struct ? pour mettre un pointeur de cellule vers la cellule suivante.
 };
 
-
-void push(struct list* pList,int value)
+void push(struct list *pList, int value)
 {
     // on met en début de liste pour l'instant
     // Etape 2
     struct cell *pc; // il faut un pointeur de cellule pour le retrouver après la sortie de la fonction.
-    pc=malloc(sizeof(struct cell));
+    pc = malloc(sizeof(struct cell));
     // Etape 3
-    pc->valeur=value;
-    pc->next=pList->firstCell;
+    pc->valeur = value;
+    pc->next = pList->firstCell;
     // Etape 4
-    pList->firstCell=pc;
+    pList->firstCell = pc;
 }
 
-int pop(struct list* pList)
+int pop(struct list *pList)
 {
     // Etape 1
     int result;
     struct cell *pc;
-    pc=pList->firstCell;
-    pList->firstCell=pc->next;
+    pc = pList->firstCell;
+    pList->firstCell = pc->next;
     // Etape 2
-    result=pc->valeur;
+    result = pc->valeur;
     free(pc);
     return result;
 }
 
-
-
 int main()
 {
     struct list l;
-    struct cell c1,c2,c3;
-    c1.valeur=3;
-    c1.next=&c2;
-    c2.valeur=15;
-    c2.next=&c3;
-    c3.valeur=7;
-    c3.next=NULL;
-    l.firstCell=&c1;
-    push(&l,5);
+    struct cell c1, c2, c3;
+    c1.valeur = 3;
+    c1.next = &c2;
+    c2.valeur = 15;
+    c2.next = &c3;
+    c3.valeur = 7;
+    c3.next = NULL;
+    l.firstCell = &c1;
+    push(&l, 5);
     printf(pop);
 
     return 0;
