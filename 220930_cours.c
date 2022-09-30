d13d95461c5dbfc6fe36f4d59cc2b1d45c241ed3
LBF38 <mathis.urien@free.fr>
2022-09-30 08:53:52 +0200

    Cours du 30/09/22 - travail sur exemples simples
    Penser à mettre free quand on fait un malloc

--- 220930_cours.c
+++ 220930_cours.c
@@ -6,10 +6,12 @@
 
 int main() // avec ou sans paramètre
 {
-    char nom[100]; //equivalent à précédemment mais sans pointeur
+    char *nom;
+    nom=(char*) malloc(100);
     printf("Entrez votre nom\n");
     scanf("%s",nom);
-    printf("hello world %s\n",nom); // nom = &(nom[0])
+    printf("hello world %s\n",nom);
+    free(nom);
     return 0; // convention pour dire que tout s'est bien passé
 }
 
