2aa88b928ccc8131790c8a9acb19d7824db9f3f4
LBF38 <mathis.urien@free.fr>
2022-09-30 08:34:07 +0200

    Cours du 30/09/22 - travail sur exemples simples
    Reprise d'exemples simples en C

---
+++ 220930_cours.c
@@ -0,0 +1,14 @@
+// Premier programme
+
+// Imports
+#include <stdio.h>
+
+int main() // avec ou sans paramètre
+{
+    printf("Hello World\n");
+    return 0; // convention pour dire que tout s'est bien passé
+}
+
+
+
+
