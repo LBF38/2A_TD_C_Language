72b5ca8f62096cae7d7664ce5e841e12fef676a8
LBF38 <mathis.urien@free.fr>
2022-09-30 08:59:50 +0200

    Cours du 30/09/22 - travail sur exemples simples
    Vérification de l'allocation de mémoire avec les malloc et free.

--- 220930_cours.c
+++ 220930_cours.c
@@ -3,15 +3,26 @@
 // Imports
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() // avec ou sans paramètre
 {
-    char *nom;
-    nom=(char*) malloc(100);
+    char *nom, *nom_court;
+    int longueur;
+    nom=(char*) malloc(10);
+    
     printf("Entrez votre nom\n");
     scanf("%s",nom);
-    printf("hello world %s\n",nom);
+
+
+    longueur = strlen(nom); // longueur de la chaîne réelle.
+    nom_court=(char*) malloc(longueur);
+
+    strcpy(nom_court,nom);
+    printf("hello world %s\n",nom_court);
+
     free(nom);
+    free(nom_court);
     return 0; // convention pour dire que tout s'est bien passé
 }
 
